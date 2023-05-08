69c25129ebf53d4c27979b495742a9fcee531d82
Laíza Milena Scheid Parizotto <41296740+laizaparizotto@users.noreply.github.com>
2023-05-08 00:23:03 -0300

    modified:   ai_tools.py 	modified:   main.py

--- ai_tools.py
+++ ai_tools.py
@@ -1,10 +1,16 @@
 from langchain.agents import Tool
-from spotify import start_playing_song_by_name, start_playing_song_by_lyrics
+from spotify import start_playing_song_by_name, start_playing_song_by_lyrics, add_song_to_queue_by_song_name, pause_music
 
 
 def tool_start_playing_song_by_name():
-    return Tool(name="Play a song given the name", func=lambda song_name: start_playing_song_by_name(song_name), description=f"""Given a song name, start playing a song. Action Input is a string of song_name.""", return_direct=True)  # type: ignore
+    return Tool(name="Song given a name", func=lambda song_name: start_playing_song_by_name(song_name), description=f"""Given a song name, start playing a song. Action Input is a string of song_name.""", return_direct=True)  # type: ignore
 
 
 def tool_start_playing_song_by_lyrics():
-    return Tool(name="Play a song given lyrics", func=lambda lyrics: start_playing_song_by_lyrics(lyrics), description=f"""Given lyrics, start playing a song. Action Input is a string of lyrics.""", return_direct=True)  # type: ignore
+    return Tool(name="Song given lyrics", func=lambda lyrics: start_playing_song_by_lyrics(lyrics), description=f"""Given lyrics, start playing a song. Action Input is a string of lyrics.""", return_direct=True)  # type: ignore
+
+def tool_add_song_to_queue_by_song_name():
+    return Tool(name="Add to queue", func=lambda song_name: add_song_to_queue_by_song_name(song_name), description=f"""Add the song to the queue. Action Input is a string of song_name.""", return_direct=True)
+
+def tool_pause_song():
+    return Tool(name="Pause music", func=lambda tool_input: pause_music(None), description="Stop music", return_direct=True)
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from ai_agents import initialize_agent_zero_shot
-from ai_tools import tool_start_playing_song_by_lyrics, tool_start_playing_song_by_name
+from ai_tools import tool_start_playing_song_by_lyrics, tool_start_playing_song_by_name, tool_add_song_to_queue_by_song_name, tool_pause_song
 
 # Initialize agent
 tools = [
     tool_start_playing_song_by_lyrics(),
     tool_start_playing_song_by_name(),
+    tool_add_song_to_queue_by_song_name(),
+    tool_pause_song(),
 ]
 
 agent = initialize_agent_zero_shot(tools=tools)
